62f105711aa79879e59e0480d0ddf1ca1712a0e3
Michal Korczynski <michal.korczynski1@gmail.com>
2026-02-12 11:05:37 +0100

    update prompt

--- cypress/e2e/amazon-site.cy.js
+++ cypress/e2e/amazon-site.cy.js
@@ -2,12 +2,10 @@ describe('Amazon site test', () => {
   it('Add to cart', () => {
     cy.prompt([
       "Given Page “https://www.amazon.com/” is open",
-      // "When [data-action-type='DISMISS'] is clicked",
       "And A user search for 'ram ddr5 32gb'",
       "And Click search icon",
       "And User open first Results item",
       "And Click 'Add to cart' button",
-      // "And If 'No thanks' button is displayed click on it",
       "Then Validate if [id='nav-cart-count'] have text '1'"
     ])
 
