e9ef74cf9c1f7d8a70b3ef99880177a54ad0764d
Michal Korczynski <michal.korczynski1@gmail.com>
2026-02-13 10:17:21 +0100

    add navigation to qa page

--- cypress/e2e/callstack.cy.js
+++ cypress/e2e/callstack.cy.js
@@ -1,7 +1,6 @@
 describe('CK site test', () => {
-
 	it('Navigate to tech tab', function() {
-    	cy.prompt([
+		cy.prompt([
 			"Visit 'https://www.callstack.com/' page",
 			"Click 'Technology' tab",
 			"Validate if 'Build for every platform within the React Universe' text is visible"
@@ -9,11 +8,21 @@ describe('CK site test', () => {
 	});
 
 	it('Open React Universe App project in case-studies', function() {
-    	cy.prompt([
+		cy.prompt([
 			"Visit 'https://www.callstack.com/' page",
 			"Click 'Case Studies' tab",
 			"Click 'View prject' button of 'React Universe App: Networking Evolved with Liquid Glass' case",
 			"Validate if 'adoption rate; majority of attendees registered in the app' text is visible"
+		])
+	});
+
+	it('Navigate to Quality Assurance page', function() {
+		cy.prompt([
+		  "Visit 'https://www.callstack.com' page",
+		  "Click 'Check our services' button",
+		  "Click 'Find out more' button under 'Enterprise Solutions'",
+		  "Click on 'Quality Assurance'",
+		  "Check if 'We help teams strengthen quality assurance in React Native apps by introducing test automation, platform-specific validation, and scalable QA processes that keep up with team and product growth.' is displayed"
 		])
 	});
 });
